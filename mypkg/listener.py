7820421f231a99ffceb2476baaec46c280f44a48
yamakouAO <yamaguchiao1031@gmail.com>
2026-01-04 12:06:33 +0900

    change command

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -12,17 +12,12 @@ node = Node("listener")
 
 def cb(msg):
     global node
-<<<<<<< HEAD
-    node.get_logger().info("Listen: %s" % msg)
-
-=======
     if msg.data > 0:
         node.get_logger().info("Remaining time: %d" % msg.data)
     elif msg.data == 0:
         node.get_logger().info("TIME OUT")
     else:
         node.get_logger().info("Over time: %d" % msg.data)
->>>>>>> dev
 
 def main():
     pub = node.create_subscription(Person, "person", cb, 10)
